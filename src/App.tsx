110fc0c9704603e2647f01eb8a81fc1f6363b8f4
stef-mat <stefanmatena@gmail.com>
2025-05-12 21:18:24 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import RadVanAvontuur from "./components/RadVanAvontuur";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-pink-100 via-yellow-100 to-blue-100">
+      <RadVanAvontuur />
+    </main>
+  );
+}
+
+export default App;
